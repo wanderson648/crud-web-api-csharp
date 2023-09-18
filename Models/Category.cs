4bc05b2d009b67f0be7e32809d07ed22e73c39bf
wanderson648 <testepedroca@gmail.com>
2023-09-18 15:47:15 -0300

    Entidades do modelo de domínio

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }    
+}
---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace APICatalogo.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set;}
+    public string? ImageUrl { get; set;}
+    public float Stock { get; set;}
+    public DateTime CreatedAt { get; set;}
+}
