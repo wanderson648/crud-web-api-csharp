e0072322c813c1495ebd3f95382f8670fe7a3974
wanderson648 <testepedroca@gmail.com>
2023-09-18 22:10:15 -0300

    Populando tabelas Categories e Product

---
+++ Migrations/20230918220527_FillCategories.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class FillCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into categories values(default, 'Lanches', 'lanches.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from categories");
+        }
+    }
+}
---
+++ Migrations/20230919004832_FillProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class FillProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into products (default, Name, Description, Price, ImageUrl, Stock, CreatedAt, CatergoryId)" +
+                "values ('Coca-cola diet', 'Refrigerante de Coca 350ml', '5.45', 'cocacola.jpg', '10', now(), 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from products");
+        }
+    }
+}
